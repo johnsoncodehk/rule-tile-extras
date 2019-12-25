531757c311ae1b9a6686bacc1880bce6a64a78ae
johnsoncodehk <johnsoncodehk@gmail.com>
2019-12-26 05:01:53 +0800

    Create WallRuleTile

---
+++ Runtime/RuleTiles/WallRuleTile.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+using System.Collections.Generic;
+
+namespace RuleTileExtras
+{
+    [CreateAssetMenu(menuName = "Rule Tile Extras/Rule Tiles/Wall Rule Tile")]
+    public class WallRuleTile : RuleTile<WallRuleTile.Neighbor>
+    {
+
+        public class Neighbor
+        {
+            public const int OtherTile = 2;
+        }
+
+        public List<TileBase> excludes = new List<TileBase>();
+
+        public override bool RuleMatch(int neighbor, TileBase other)
+        {
+            if (other is RuleOverrideTile)
+                other = (other as RuleOverrideTile).m_InstanceTile;
+
+            switch (neighbor)
+            {
+                case Neighbor.OtherTile:
+                    return other && other != this && !excludes.Contains(other);
+            }
+
+            return base.RuleMatch(neighbor, other);
+        }
+    }
+}
